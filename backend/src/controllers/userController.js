cadac72e8c2e9c8805410b301f4631124d538199
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-07 17:39:36 +0530

    Refactor userController by commenting out unused Todo functions
    - Commented out updateTodo, updateStatus, and deleteTodo functions to streamline the userController.
    - Removed references to unused functions in the module exports to improve code clarity and maintainability.

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -20,7 +20,53 @@ const createUser = async({username,email,hashedPassword})=>{
     }
 }
 
+// const updateTodo = async(id,{title,category,priority,dueDate,status}) =>{
+//     try{
+//         const response = await Todo.findByIdAndUpdate(id,
+//             {$set:{
+//                     title:title,
+//                     category:category,
+//                     priority:priority,
+//                     dueDate:dueDate,
+//                     status:status
+//                 }
+//             },
+//             {new:true})
+//         return response._id;
+//     }catch(error){
+//         console.log("error during updating the todo! "+error)
+//         return;
+//     }
+// }
+
+// const updateStatus = async(id,status) =>{
+//     try{
+//         const response = await Todo.findByIdAndUpdate(id,{
+//             $set:{status:status}
+//         })
+//         return response;
+//     }
+//     catch(error){
+//             console.log("error during updating the status! "+error);
+//             return;
+//     }
+// }
+// const deleteTodo = async(id)=>{
+//     try{
+//         const response = await Todo.findByIdAndDelete(id);
+//         return response._id;
+//     }catch(error){
+//         console.log("error during updating the todo! "+error)
+//         return;
+//     }
+// }
 
 module.exports = {
     createUser,
+    // markEmailVerified
+    // getTodo,
+    // getTodos,
+    // updateTodo,
+    // updateStatus,
+    // deleteTodo
 }
